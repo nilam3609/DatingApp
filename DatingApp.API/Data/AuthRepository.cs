d4760962faae0c9fc7d045f320dce3b8181d5990
NilamPatel <nilam3609@gmail.com>
2020-12-04 17:51:01 +0100

    Init

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x =>x.Username == username);
+            if(user == null)
+                return null;
+            
+            if(!VerifyPasswordHash(password, user.PasswordHash , user.PasswordSalt))
+            return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+             using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+           {
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+               for(int i = 0 ; i< computedHash.Length; i++)
+               {
+                   if(computedHash[i] != passwordHash[i]) return false;
+               }
+           }
+           return true;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash , passwordSalt ;
+            //used keyword out so that once the value is update in the function it would also reflect here.
+            CreatePasswordHash(password, out passwordHash,out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return (user);
+
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //added inside the using statement because Cryptography disposes the method once its completed.
+           using(var hmac = new System.Security.Cryptography.HMACSHA512())
+           {
+               passwordSalt = hmac.Key;
+               passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+           }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if(await _context.Users.AnyAsync(x => x.Username == username)) 
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Controllers;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember( dest => dest.Age , opt => {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+
+            CreateMap<User, UserForDetailDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember( dest => dest.Age , opt => {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+
+            CreateMap<Photo, PhotosForDetailDto>();
+            CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();  
+            CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>();
+            CreateMap<Message,MessageToReturnDto>()
+            .ForMember(m => m.SenderPhotoUrl, opt => opt
+                .MapFrom(u =>u.Sender.Photos.FirstOrDefault(p =>p.IsMain).Url))
+            .ForMember(m => m.RecipientPhotoUrl, opt => opt
+                .MapFrom(u =>u.Recipient.Photos.FirstOrDefault(p =>p.IsMain).Url));
+
+        }
+    }
+}
